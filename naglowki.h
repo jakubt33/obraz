4560bdf2c63732e1ac6331c2cf8bc3380e3880ef
jakubt33 <jakub.trzyna@gmail.com>
2013-12-12 13:36:47 +0100

    dodano odczyt ilosci odcieni

--- naglowki.h
+++ naglowki.h
@@ -6,20 +6,22 @@
 #include <time.h>
 
 #define MAXCOMMENT 100
-#define B_AND_W 1
-#define GR_SCALE 2
-#define COLOR 3
+#define B_AND_W 49
+#define GR_SCALE 50
+#define COLOR 51
 #define MAGIC_NUMBER 1
 #define BAD_FILE_TYPE -1
 #define TYPE_OK 1
 #define DIM_OK 1
-
+#define DIM_FAIL -1
+#define STOP -1
+#define WORK 1
 
 typedef struct element
 {
     struct element *next;
     char type[2+1];
-    int data;
+    int odcienie;
     char wymiary[2+1]; // macierz [x,y]
     char comment[MAXCOMMENT];
 } element;
@@ -28,13 +30,17 @@ element *push(element *, element *);
 void wyswietl(element *);
 element * usun(element *);
 element * wczytajobraz(element*);
-void sprawdz_komentarz(FILE *pFile);
+void sprawdz_komentarz(FILE *pFile, element *temp);
 int sprawdz_typ(FILE *pFile, element *temp);
 int sprawdz_wymiary(FILE *pFile, element *temp);
+int sprawdz_odcienie(FILE *pFile, element *temp);
+int pobierz_obraz(FILE *pFile, element *temp);
+void pobierz_tablice(FILE *pFile, element *temp);
 
 
 element * wczytajobraz(element *lista)
 {
+    int dzialaj = WORK;
     element *temp;
     temp=(element*)malloc(sizeof(element));
 
@@ -47,26 +53,34 @@ element * wczytajobraz(element *lista)
     }
     else
     {
-        sprawdz_komentarz(pFile);
+        sprawdz_komentarz(pFile, temp);
 
-        if ( (sprawdz_typ(pFile, temp)) == TYPE_OK ) //odczytany typ
+        if ( (sprawdz_typ(pFile, temp)) == TYPE_OK )
         {
-            sprawdz_komentarz(pFile);
+            sprawdz_komentarz(pFile, temp);
 
             if( sprawdz_wymiary(pFile, temp) == DIM_OK )
             {
-                sprawdz_komentarz(pFile);
+                sprawdz_komentarz(pFile, temp);
+
+                dzialaj = pobierz_obraz(pFile, temp);
                 //to fajnie, mozna przejsc do pobierania danych do tablicy
             }
-            else ;//flaga error
+            else
+                dzialaj=STOP; //flaga error
+        }
+        else
+            dzialaj=STOP; //flaga error
 
-        } //odczytrany typ i magic number, flaga ze mamy typ i magic number
+        if( dzialaj == WORK )
+        {
+            temp->next=NULL;
+            lista=push(lista, temp);
+            printf("dodano obraz\n");
+        }
+        else if ( dzialaj == STOP )
+            printf("program napotkał błąd przy odczycie danych obrazu\n");
 
-        else ;//flaga error
-
-        temp->next=NULL;
-        lista=push(lista, temp);
-        printf("dodano obraz\n");
 
     }
 
@@ -75,12 +89,65 @@ element * wczytajobraz(element *lista)
 
     return lista;
 }
+int pobierz_obraz(FILE *pFile, element *temp)
+{
+    switch ( temp->type[MAGIC_NUMBER] )
+    {
+    case B_AND_W:
+
+        pobierz_tablice(pFile, temp);
+        return WORK;
+        break;
+
+    case GR_SCALE:
+
+        sprawdz_komentarz(pFile, temp);
+        if( sprawdz_odcienie(pFile, temp) )
+        {
+            pobierz_tablice(pFile, temp);
+            return WORK;
+        }
+        else
+        {
+            return STOP;
+        }
+        break;
+
+    case COLOR:
+
+        printf("program nie umozliwia jeszcze obsługi kolorowych obrazów\n"); //mozna bedzei dodac funcke jak starczy czasu\
+        return STOP;
+        break;
+
+    default:
+        return STOP;
+
+    }
+}
+int sprawdz_odcienie(FILE *pFile, element *temp)
+{
+    if ( fscanf( pFile, "%d", &temp->odcienie) )
+    {
+        return WORK;
+    }
+    return STOP;
+}
+void pobierz_tablice(FILE *pFile, element *temp)
+{
+
+}
 int sprawdz_wymiary(FILE *pFile, element *temp)
 {
-    fscanf(pFile, "%d", &temp->wymiary[0]);
-    fscanf(pFile, "%d", &temp->wymiary[1]);
-    temp->wymiary[2] = '\0';
-    return DIM_OK; //or no // dopisac
+    if (fscanf(pFile, "%d", &temp->wymiary[0]))
+    {
+        if (fscanf(pFile, "%d", &temp->wymiary[1]))
+        {
+            temp->wymiary[2] = '\0';
+            return DIM_OK; //or no // dopisac
+        }
+        else return DIM_FAIL;
+    }
+    else return DIM_FAIL;
 }
 int sprawdz_typ(FILE *pFile, element *temp)
 {
@@ -89,7 +156,7 @@ int sprawdz_typ(FILE *pFile, element *temp)
     if(temp->type[0] == 'P')
     {
         temp->type[MAGIC_NUMBER] = fgetc(pFile);
-        if( (temp->type[MAGIC_NUMBER] == '1') || (temp->type[MAGIC_NUMBER] == '2') || (temp->type[MAGIC_NUMBER] == '3') )
+        if( (temp->type[MAGIC_NUMBER] == B_AND_W) || (temp->type[MAGIC_NUMBER] == GR_SCALE) || (temp->type[MAGIC_NUMBER] == COLOR) )
         {
             temp->type[2] = '\0';
             return TYPE_OK;
@@ -98,13 +165,13 @@ int sprawdz_typ(FILE *pFile, element *temp)
     }
     else return BAD_FILE_TYPE;
 }
-void sprawdz_komentarz(FILE *pFile)
+void sprawdz_komentarz(FILE *pFile, element *temp)
 {
     char test;
     if( (test = fgetc(pFile)) == '#')
     {
         // zspisuje linie komentarza()
-        sprawdz_komentarz(pFile);
+        sprawdz_komentarz(pFile, temp);
     }
     else
     {
@@ -132,7 +199,7 @@ void wyswietl(element *first)
     {
         do
         {
-            printf("\ntyp obrazu to %s\nwymiary to %d na %d\n", first->type, first->wymiary[0], first->wymiary[1]);
+            printf("\ntyp obrazu to %s\nwymiary to %d na %d\nilosc odcieni to %d\n", first->type, first->wymiary[0], first->wymiary[1], first->odcienie);
             first=first->next;      //modyfikuijmy tylko kopie wskaznika!
         }
         while(first!=NULL);
